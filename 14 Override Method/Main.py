041b210b84d6af4280d8e40d213043365b5e0b29
Aris Rusliana <aris.rusliana@gmail.com>
2024-10-15 21:20:45 +0700

    Latihan OOP Python

---
+++ 14 Override Method/Main.py
@@ -0,0 +1,36 @@
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+    
+    def showInfo(self):
+        print('show info superclass Hero')
+        print ('{} health : {}'.format(
+            self.name,
+            self.health
+            )
+        )
+    
+class Hero_intelligent(Hero):
+    def __init__(self, name):
+        super().__init__(name, 100)
+    
+    # override
+    def showInfo(self):
+        print('show info subclass Hero_intelligent')
+        print ('{}\n\ttipe : intelligent,\n\thealth : {}'.format(
+            self.name,
+            self.health
+        ))
+
+class Hero_strength(Hero):
+    def __init__(self, name):
+        super().__init__(name, 200)
+
+lina = Hero_intelligent('lina')
+axe = Hero_strength('axe')
+
+lina.showInfo()
+axe.showInfo()
+
+
