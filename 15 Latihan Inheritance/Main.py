08bd731a0aa72708ed4644662b5862d4ba68b53f
Aris Rusliana <aris.rusliana@gmail.com>
2024-10-16 20:37:17 +0700

    Latihan OOP Python

---
+++ 15 Latihan Inheritance/Main.py
@@ -0,0 +1,14 @@
+from Hero import HeroIntelligent, HeroStrength
+
+lina = HeroIntelligent('lina')
+axe = HeroStrength('axe')
+
+lina.show_info()
+axe.show_info()
+
+lina.gainExp = 200
+axe.gainExp = 250
+
+lina.show_info()
+axe.show_info()
+
